078855319de8ae44786408649d8ca9ee8bcb7e5e
ThaiPy0605 <xuanthainguyen652005@gmail.com>
2025-11-05 23:11:51 +0700

    Add filter by Category for guest and customer page, fix logic update IMG for product

--- src/main/java/hsf302/hsf302project/config/WebConfig.java
+++ src/main/java/hsf302/hsf302project/config/WebConfig.java
@@ -9,6 +9,6 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/images/**")
-                .addResourceLocations("file:///D:/FlowerImg/");
+                .addResourceLocations("file:///C:/FlowerImg/");
     }
 }
